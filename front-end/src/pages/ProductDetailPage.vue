b34f17bd61bac266c1156156a2d768b034b1a7f3
Riocodex <onwukachibike@gmail.com>
2024-09-17 14:06:59 +0200

    updated

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,10 +1,17 @@
 <template>
     <h1> Product Detail Page   </h1>
+    
 </template>
 
 
 <script>
+import { products } from '../temp-data';
     export default {
         name: "ProductDetailPage",
+        data(){
+            return{
+                product: products.find(product => product.id === this.$route.params.productId),
+            }
+        }
     }
 </script>
