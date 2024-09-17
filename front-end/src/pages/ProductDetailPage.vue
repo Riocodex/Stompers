e2fe54bc947f11480649988c1a791190b2256bab
Riocodex <onwukachibike@gmail.com>
2024-09-17 14:20:16 +0200

    productdetails page working

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,6 +1,13 @@
 <template>
-    <h1> Product Detail Page   </h1>
-    
+    <div class="img-wrap">
+    <img :src="product.imageName"/>
+    </div>
+
+    <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add to cart</button>
+    </div>
 </template>
 
 
