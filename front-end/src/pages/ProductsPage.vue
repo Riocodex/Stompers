cfa382136a3e5ff3e6b9bcd973cef563e72e1990
Riocodex <onwukachibike@gmail.com>
2024-09-17 13:58:46 +0200

    updated

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1> Products Page   </h1>
+    <h1>Products</h1>
+    <div class="grid-wrap">
+        <div
+        class="product-item"
+         v-for="product in products"
+         :key="product.id"
+         >
+            <img :src="product.imageName"/>
+            <h3 class="product-name">{{ product.name }}</h3>
+            <p class="product-price">{{ product.price }}</p>
+            <button>View Details</button>
+        </div>
+        
+    </div>
+    
 </template>
 
 
 <script>
-    export default {
-        name: "ProductsPage",
-    }
+import  { products } from '../temp-data'
+   export default {
+  name: "ProductsPage",
+  data() {
+    return {
+      products // This needs to be returned from the data function
+    };
+  }
+};
 </script>
