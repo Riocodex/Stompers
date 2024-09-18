4dc9a05a31e9c89b3de056a4a7f6c3c1275a7fa8
Riocodex <onwukachibike@gmail.com>
2024-09-18 12:02:45 +0200

    ypdated

---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="product-container" v-for="product in products" :key="product.id">
+      <img class="product-image" :src="product.imageName" />
+
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+   
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartList",
+  props: ['products'],  // 'products' is passed as a prop, not 'cartItems'
+}
+</script>
--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,16 +1,8 @@
 <template>
   <h1>Shopping Cart</h1>
   <div v-if="cartItems.length > 0">
-    <div class="product-container" v-for="product in cartItems" :key="product.id">
-      <!-- No need for require here -->
-      <img class="product-image" :src="product.imageName" />
-
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>{{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove from Cart</button>
-    </div>
+   <ShoppingCartList :products="cartItems"/>
+   
     <button class="checkout-button">Proceed to Checkout</button>
   </div>
   <div v-if="cartItems.length === 0">
@@ -19,10 +11,14 @@
 </template>
 
 <script>
+import ShoppingCartList from '@/components/ShoppingCartList.vue'
 import { cartItems } from '../temp-data';
 
 export default {
   name: "ShoppingCartPage",
+  components:{
+    ShoppingCartList,
+  },
   data() {
     return {
       cartItems,  // Use the cartItems from temp-data with resolved image paths
