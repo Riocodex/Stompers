02725a17ea7212739762298b4ce25769e2055676
Riocodex <onwukachibike@gmail.com>
2024-09-18 11:12:46 +0200

    linked te product details

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -9,7 +9,10 @@
       <img :src="product.imageName" />
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
-      <button>View Details</button>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
+      
     </div>
   </div>
 </template>
