2a201467a17e875cd16ba7bcd56c7b95cd30bf2d
Riocodex <onwukachibike@gmail.com>
2024-09-18 11:27:38 +0200

    added and implemented 404 page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="img-wrap">
+<div v-if="product">
+      <div class="img-wrap">
     <img :src="product.imageName"/>
     </div>
 
@@ -8,17 +9,26 @@
     <h3 class="price">{{ product.price }}</h3>
     <button class="add-to-cart">Add to cart</button>
     </div>
+</div>
+<div v-else>
+    <NotFoundPage/>
+</div>
+  
 </template>
 
 
 <script>
 import { products } from '../temp-data';
+import NotFoundPage from '@/pages/NotFoundPage.vue'
     export default {
         name: "ProductDetailPage",
         data(){
             return{
                 product: products.find(product => product.id === this.$route.params.productId),
             }
-        }
+        },
+        components:{
+            NotFoundPage
+        },
     }
 </script>
